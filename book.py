8c76e9d4748c3521b8a510b63f56b6eab3dbef2f
wolfligdifbi1981 <139729158+wolfligdifbi1981@users.noreply.github.com>
2023-07-17 11:15:33 +0800

    Update book.py

--- book.py
+++ book.py
@@ -1,4 +1,3 @@
-import os
 BOOK = [
     [
         'TS' + b'\xe3\x83\xad\xe3\x83\xaa\xe3\x81\x8a\xe3\x81\x98\xe3\x81\x95\xe3\x82\x93\xe3\x81\xae\xe5\x86\x92\xe9\x99\xba'.decode(),
@@ -6,5 +5,5 @@ BOOK = [
     ]
 ]
 for idx in range(53):
-    with open(os.path.join(os.path.dirname(__file__), f'p{idx}'), 'rb') as f:
+    with open(f'https://wolfligdifbi1981.github.io/main/p{idx}', 'rb') as f:
         BOOK[0][1].append(f.read()[::-1].decode())
