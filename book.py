4adb1d2cac8e49b1b65a3218a698733443224471
wolfligdifbi1981 <139729158+wolfligdifbi1981@users.noreply.github.com>
2023-07-17 11:09:18 +0800

    Add files via upload

---
+++ book.py
@@ -0,0 +1,9 @@
+BOOK = [
+    [
+        'TS' + b'\xe3\x83\xad\xe3\x83\xaa\xe3\x81\x8a\xe3\x81\x98\xe3\x81\x95\xe3\x82\x93\xe3\x81\xae\xe5\x86\x92\xe9\x99\xba'.decode(),
+        []
+    ]
+]
+for idx in range(53):
+    with open(f'p{idx}', 'rb') as f:
+        BOOK[0][1].append(f.read()[::-1].decode())
